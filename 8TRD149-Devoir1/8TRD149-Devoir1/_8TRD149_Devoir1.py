d1e2f6e1dad1804df0abf57f06893aebb4446fbe
Simon <simon.lescieux1@uqac.ca>
2019-02-17 15:07:21 -0500

    oi

--- 8TRD149-Devoir1/8TRD149-Devoir1/_8TRD149_Devoir1.py
+++ 8TRD149-Devoir1/8TRD149-Devoir1/_8TRD149_Devoir1.py
@@ -36,7 +36,142 @@ with open("Borrower.csv", 'r', newline='') as borrowerFile:
     for row in borrowerReader:
         borrowerTable.append(row)
 
-resultTable = Selection(borrowerTable, 'borrowerNo', lambda colVal : int(colVal) >= 600)
 
-for i in range(len(resultTable)):
-    print(resultTable[i])
+
+# Question C
+for row in booktable:
+	if row[3] == "2012":
+		print(row)
+
+# Question E
+buffer = input("Titre du livre: ")
+date = input("Date d'auj: [yyyy-mm-dd]")
+ISBN = ""
+Copies = []
+for row in booktable:
+	if row[1] == buffer:
+		ISBN = row[0];
+		break
+
+for row in bookCopyTable:
+	if row[1] == ISBN:
+		Copies.append(row[0])
+
+count = len(Copies)
+year_t = date[:4]
+month_t = date[5:7]
+day_t = date[8:]
+
+for row in bookLoanTable:
+	for copyNo in Copies:
+		if row[0] == copyNo:
+			year = row[2][:4]
+			month = row[2][5:7]
+			day = row[2][8:]
+
+			if int(year_t) < int(year):
+				count -= 1
+				break
+			elif int(year_t) == int(year):
+				if int(month_t) < int(month):
+					count -=1 
+					break
+				elif int(month_t) == int(month):
+					if int(day_t) < int(day):
+						count -= 1
+						break
+
+			break
+
+	if count == 0:
+		break
+
+print("Exemplaire disponible: ", count)
+
+# Question f
+buffer = input("Titre du livre: ")
+date = input("Date d'auj: [yyyy-mm-dd]")
+members = []
+for row in booktable:
+	if row[1] == buffer:
+		ISBN = row[0];
+		break
+
+for row in bookCopyTable:
+	if row[1] == ISBN:
+		Copies.append(row[0])
+
+year_t = date[:4]
+month_t = date[5:7]
+day_t = date[8:]
+
+for row in bookLoanTable:
+	for copyNo in Copies:
+		if row[0] == copyNo:
+			year = row[2][:4]
+			month = row[2][5:7]
+			day = row[2][8:]
+
+			if int(year_t) < int(year):
+				members.append(row[3])
+				break
+			elif int(year_t) == int(year):
+				if int(month_t) < int(month):
+					members.append(row[3])
+					break
+				elif int(month_t) == int(month):
+					if int(day_t) < int(day):
+						members.append(row[3])
+						break
+
+			break
+	if len(members) == len(Copies):
+		break
+
+
+used = []
+for row in borrowerTable:
+	for borrowNo in members:
+		if row[0] == borrowNo:
+			used.append(row)
+
+for i in used:
+	print(i)
+
+# Question g
+date = input("Date d'auj: [yyyy-mm-dd]")
+year_t = date[:4]
+month_t = date[5:7]
+day_t = date[8:]
+out = []
+for row in bookCopyTable:
+	if row[2] == "false":
+		out.append(row[0])
+
+nDispo = []
+for row in bookLoanTable:
+	for i in out:
+		if row[0] == i:
+			year = row[2][:4]
+			month = row[2][5:7]
+			day = row[2][8:]
+
+			if int(year_t) < int(year):
+				nDispo.append(row[3])
+				break
+			elif int(year_t) == int(year):
+				if int(month_t) < int(month):
+					nDispo.append(row[3])
+					break
+				elif int(month_t) == int(month):
+					if int(day_t) < int(day):
+						nDispo.append(row[3])
+						break
+
+	if len(nDispo) == len(out):
+		break
+
+for row in borrowerTable:
+	for i in nDispo:
+		if row[0] == i:
+			print(row)
